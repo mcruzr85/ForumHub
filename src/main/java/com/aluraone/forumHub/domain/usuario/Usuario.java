4babf4cb17c4a22ded7c3d45bb94140b612e5777
mcruzr85 <mcruzrodriguez1985@gmail.com>
2024-07-16 02:37:12 -0300

    script banco dados

--- src/main/java/com/aluraone/forumHub/domain/usuario/Usuario.java
+++ src/main/java/com/aluraone/forumHub/domain/usuario/Usuario.java
@@ -27,7 +27,6 @@ public class Usuario implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String nome;
   private String email;
   private String senha;
